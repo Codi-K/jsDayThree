890411528fda5d952db13f0edf0d9dd011fc7a47
Codi-K <codikader7@gmail.com>
2023-05-17 09:35:27 +0200

    day 3 exercise

---
+++ script/code.js
@@ -0,0 +1,36 @@
+// Question 1
+
+let firstName = "Codi";
+console.log( firstName);
+console.log(typeof firstName);
+
+// Question 2
+
+firstName = 22;
+console.log(firstName);
+console.log(typeof firstName);
+
+// Question 3
+
+let surName = "Kader";
+console.log(surName);
+console.log(typeof surName);
+
+let numb1 = 2;
+console.log(numb1);
+console.log(typeof numb1);
+
+let statement = true;
+console.log(statement);
+console.log(typeof statement);
+
+let ball = null;
+console.log(ball);
+console.log(typeof ball);
+
+let color;
+console.log(color);
+
+let games = ["Apex Legends","Call of Duty","Fortnite","Valorant"];
+console.log(games);
+console.log(typeof games);
